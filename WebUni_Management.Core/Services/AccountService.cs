8c7ea3101ea59555fcc52cd4d1667a0dda5a2df0
yassyolo <yoana.yotova03@gmail.com>
2024-03-09 02:49:05 +0200

    service for the accounts management made

---
+++ WebUni_Management.Core/Services/AccountService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebUni_Management.Core.Contracts;
+using WebUni_Management.Infrastructure.Repository;
+
+namespace WebUni_Management.Core.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IRepository repository;
+
+        public AccountService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+    }
+}
