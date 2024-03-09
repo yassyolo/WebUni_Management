84eb3f4ff5396ec2d9001a616dc32b902c32cc8b
yassyolo <yoana.yotova03@gmail.com>
2024-03-09 02:00:23 +0200

    repository made that uses the application database context

---
+++ WebUni_Management.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebUni_Management.Data;
+
+namespace WebUni_Management.Infrastructure.Repository
+{
+    public class Repository : IRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public Repository(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await context.Set<T>().AddAsync(entity);
+        }
+        public IQueryable<T> All<T>() where T : class
+        {
+            return GetDbSet<T>();
+        }
+
+        private IQueryable<T> GetDbSet<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return GetDbSet<T>().AsNoTracking();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+    }
+}
