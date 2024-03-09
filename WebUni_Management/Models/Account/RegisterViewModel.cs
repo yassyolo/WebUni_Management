9d2e5be28247cee16ee04c7986ae0325edb4ce80
yassyolo <yoana.yotova03@gmail.com>
2024-03-09 02:48:17 +0200

    view model for registration made

---
+++ WebUni_Management/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static WebUni_Management.Models.Constants.MessageConstants;
+
+using static WebUni_Management.Infrastructure.Data.Constants.ModelConstants;
+
+namespace WebUni_Management.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [EmailAddress(ErrorMessage = InvalidEmailErrorMessage)]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(FacultyNumberLength, ErrorMessage = UserNameLengthErrorMessage)]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
