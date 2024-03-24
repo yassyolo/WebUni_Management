5241295c616cae8082ad3303b279fe8d18fe57ce
yassyolo <yoana.yotova03@gmail.com>
2024-03-24 20:04:20 +0200

    New views added

---
+++ WebUni_Management.Core/Models/News/NewsDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebUni_Management.Core.Models.News
+{
+    public class NewsDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set;} = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string PublishedOn { get; set; } = string.Empty;
+    }
+}
