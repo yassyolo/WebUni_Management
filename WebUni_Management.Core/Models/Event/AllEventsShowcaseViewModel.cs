1e13d3a9367a60d8f0aff72ec55d9d81b8c9135e
yassyolo <yoana.yotova03@gmail.com>
2024-03-27 21:16:25 +0200

    New views

---
+++ WebUni_Management.Core/Models/Event/AllEventsShowcaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebUni_Management.Core.Models.Event
+{
+	public class AllEventsShowcaseViewModel
+	{
+		public int EventsPerPage { get; set; } = 2;
+
+		public int CurrentPage { get; set; } = 1;
+		public string SearchTerm { get; set; } = string.Empty;
+        public int TotalEvents { get; set; }
+		public IEnumerable<EventIndexViewModel> Events { get; set; } = new List<EventIndexViewModel>();
+    }
+}
